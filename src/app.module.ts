840c7696f9f26745502797d26bfd68cc3a348982
Guilherme Faxina <guilhermefaxina@MacBook-Air-de-Guilherme.local>
2025-06-12 22:27:49 -0300

    feat(config): create config module and set prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { z } from 'zod';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      validationSchema: z.object({
+        POSTGRES_USER: z.string(),
+        POSTGRES_PASSWORD: z.string(),
+        POSTGRES_DB: z.string(),
+        DATABASE_URL: z.string(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
